9df3cb2557ace3c0877d2d260d26e6b85405361a
Yoann FUKS <yfuks@student.42.fr>
2016-09-07 14:33:53 +0200

    add reload intro

--- js/introduction.js
+++ js/introduction.js
@@ -1,6 +1,7 @@
 var videoAcceuil = document.getElementsByClassName("fillWidth")[0];
 var videoIntro = document.getElementsByClassName("fillWidth2")[0];
 var gameInfos = document.getElementsByClassName("game-infos")[0];
+var buttonReloadIntro = document.getElementsByClassName("button-reload")[0];
 
 var vidEnded = false;
 
@@ -11,14 +12,24 @@ videoIntro.onended = function() {
 videoAcceuil.onclick = function (e) {
   videoAcceuil.pause();
   videoAcceuil.style.display = "none";
-  videoIntro.play();
+  playIntro();
+}
+
+buttonReloadIntro.onclick = function (e) {
+  playIntro();
+}
+
+function playIntro() {
   videoIntro.style.display = "block";
+  videoIntro.play();
+  gameInfos.style.display = "none";
 
   var timer = setInterval(function () {
     var time = videoIntro.currentTime;
       if (vidEnded) {
           clearInterval(timer);
           gameInfos.style.display = "block";
+          vidEnded = false; // put to false for reload purpose
       }
   }, 700);
 }
